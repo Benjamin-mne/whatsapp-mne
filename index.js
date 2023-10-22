fc2361bda700c89ae015cd1c22378b6b7c5a1260
mnesicles <benjamindelgado.contacto@gmail.com>
2023-10-21 21:42:45 -0300

    adding cat-flow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import app from "./src/app.js"
 import { banner } from './src/utils/utils.js';
 import { main } from './src/botMain.js'
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
     console.log(`Server express running on port: ${port}`)
--- src/botMain.js
+++ src/botMain.js
@@ -1,14 +1,68 @@
 import botWhatsappp from '@bot-whatsapp/bot'
 import BaileysProvider from '@bot-whatsapp/provider/baileys'
 import MockAdapter from '@bot-whatsapp/database/mock'
+import fs from 'node:fs'
+import axios from 'axios'
 
 const { createBot, createProvider, createFlow, addKeyword } = botWhatsappp
 
-const flowPrincipal = addKeyword(['!hola' ]).addAnswer('¡Hola! 👁️')
+const catObject = {
+    tag: 'cat',
+    msg: ' '
+}
+
+const getRandomCat = async (tag, msg) => {
+    try {
+        const res = await axios.get(`https://cataas.com/cat/${tag}`, {
+            responseType: 'stream'
+        });
+        
+        const imagePath = 'cat.jpg'; 
+        const imageStream = fs.createWriteStream(imagePath);
+        res.data.pipe(imageStream);
+
+        await new Promise((resolve) => {
+            imageStream.on('finish', resolve);
+        });
+
+
+        console.log(tag, msg)
+
+        return imagePath;
+    } catch (error) {
+        console.error('error getting kitten image :(', error);
+        return null;
+    }
+}
+
+const flowCat = 
+    addKeyword(['!cat', '!catAngry', '!catSad', '!catCute'])
+    .addAction( async (ctx) => {
+        if (ctx.body.includes("!cat")) {
+            catObject.tag = 'cat';
+            catObject.msg = '🐈 🐈 🐈';
+        }
+        if (ctx.body.includes("!catCute")) {
+            catObject.tag = 'cute';
+            catObject.msg = '^•ﻌ•^ฅ♡ ⋆⭒˚｡⋆';
+        }  
+        if (ctx.body.includes("!catAngry")) {
+            catObject.tag = 'angry';
+            catObject.msg = '😾 😾 😾';
+        }  
+        if (ctx.body.includes("!catSad")) {
+            catObject.tag = 'sad';
+            catObject.msg = '😿 😿 😿';
+        } 
+
+        await getRandomCat(catObject.tag, catObject.msg)  
+
+    })
+    .addAnswer(catObject.msg, { media: './cat.jpg', sensitive: true  })
 
 export const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    const adapterFlow = createFlow([flowCat])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
